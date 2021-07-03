b28df7486d55449a4a9c2fff0fb2dad938d764e6
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-07-03 16:53:28 +0700

    Update ssh1.sh

--- debian10/ssh1.sh
+++ debian10/ssh1.sh
@@ -176,29 +176,41 @@ echo "========================================================="
 cd
 # buat directory badvpn
 echo "================= Auto Installer Disable badVPN V 3  ======================"
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://adiscript.vercel.app/vpn/badvpn-udpgw64"
+chmod +x /usr/bin/badvpn-udpgw
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500' /etc/rc.local
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+
+
 # buat directory badvpn
-cd /usr/bin
-mkdir build
-cd build
-wget https://github.com/ambrop72/badvpn/archive/1.999.130.tar.gz
-tar xvzf 1.999.130.tar.gz
-cd badvpn-1.999.130
-cmake -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1
-make install
-make -i install
+#cd /usr/bin
+#mkdir build
+#cd build
+#wget https://github.com/ambrop72/badvpn/archive/1.999.130.tar.gz
+#tar xvzf 1.999.130.tar.gz
+#cd badvpn-1.999.130
+#cmake -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1
+#make install
+#make -i install
 
 # auto start badvpn single port
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 100' /etc/rc.local
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500 --max-connections-for-client 100 &
-cd
+#sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 100' /etc/rc.local
+#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500 --max-connections-for-client 100 &
+#cd
 
 # permition
-chmod +x /usr/local/bin/badvpn-udpgw
-chmod +x /usr/local/share/man/man7/badvpn.7
-chmod +x /usr/local/bin/badvpn-tun2socks
-chmod +x /usr/local/share/man/man8/badvpn-tun2socks.8
-chmod +x /usr/bin/build
-chmod +x /etc/rc.local
+#chmod +x /usr/local/bin/badvpn-udpgw
+#chmod +x /usr/local/share/man/man7/badvpn.7
+#chmod +x /usr/local/bin/badvpn-tun2socks
+#chmod +x /usr/local/share/man/man8/badvpn-tun2socks.8
+#chmod +x /usr/bin/build
+#chmod +x /etc/rc.local
 
 # Custom Banner SSH
 
